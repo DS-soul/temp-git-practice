b14200f56005c8b6036be7e2e8997f1dc9c91454
Mahdi Moushekhian <mahdi.moushekhian@gmail.com>
2024-12-03 11:26:09 +0330

    add testcase for main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import numpy as np
 from utils import add, opps
 
-print(add(3, 4))
+def test():
+    return 'test'
 
-print(add(5, 6))
-print(opps(3, 4))
-# this is a test for remote renamig
+assert test() == 'test'
