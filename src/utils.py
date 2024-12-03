85fc7749448902ad3652c68ad3a823ecead12999
Mahdi Moushekhian <mahdi.moushekhian@gmail.com>
2024-12-03 11:41:16 +0330

    Add div, mul functions and edit opps func

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,13 @@ def add(a, b):
     return a + b
 
 def opps(a, b):
-    return 10
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
 
 # this is a comment for test token
 # this is another test
