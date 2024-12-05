a4fa8b1868ca4ce87af7a61481b3faf157a32ad1
Mahdi Moushekhian <mahdi.moushekhian@gmail.com>
2024-12-05 11:34:21 +0330

    Update in main branch

--- src/main.py
+++ src/main.py
@@ -1,13 +1,4 @@
 import numpy as np
 from utils import add, sub, mul, power, div, sqrut
 
-# Add more funcs
-
-print('hello world!')
-print('Hi again from main')
-def main():
-    print('Mahdi')
-
-if __name__ == '__main__':
-    main()
-    print(add(3, 4))
+print('Siamak Mushakhian')
